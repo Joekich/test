134046e87e1a5f7c5babba0bbd56b19ad21077ce
Evgeniy Filippov <Shelupons@gmail.com>
2025-01-20 01:11:19 +0300

    refactor: eventlisteners

--- practice/src/scripts/modal.js
+++ practice/src/scripts/modal.js
@@ -6,46 +6,43 @@ const logoUpdateBlock = document.getElementById('form__logo-update');
 const logoImage = document.getElementById('logo-image');
 const logoUploadInput = document.getElementById('logo-upload');
 const logoDeleteButton = document.getElementById('form__logo-delete-bg');
-const defaultLogo = 'practice/src/assets/image/default-profile-image.webp';
 const submitButton = document.getElementById('form__submit-button');
 const organizationInput = document.getElementById('organization-input');
 const phoneInput = document.getElementById('phone-input');
 const emailInput = document.getElementById('email-input');
 const selectInput = document.getElementById('form__select-input');
 
+const defaultLogo = 'practice/src/assets/image/default-profile-image.webp';
+const defaultLogoSrc = 'practice/src/assets/image/default-profile-image.webp';
+
 let isOrganizationValid = false;
 let isPhoneValid = false;
 let isEmailValid = false;
 let isSelectValid = false;
 let isLogoValid = false;
-let scrollbarWidth = 0;
-const defaultLogoSrc = 'practice/src/assets/image/default-profile-image.webp';
 
 function getScrollbarWidth() {
     const div = document.createElement('div');
     div.style.visibility = 'hidden';
     div.style.overflow = 'scroll';
     document.body.appendChild(div);
-    scrollbarWidth = div.offsetWidth - div.clientWidth;
+    const scrollbarWidth = div.offsetWidth - div.clientWidth;
     document.body.removeChild(div);
+    return scrollbarWidth;
 }
 
 function openModal() {
-    getScrollbarWidth();
+    const scrollbarWidth = getScrollbarWidth();
     modal.classList.add('modal--visible');
     document.body.style.overflow = 'hidden';
     document.body.style.paddingRight = `${scrollbarWidth}px`;
-    validateOrganization();
-    validatePhone();
-    validateEmail();
-    validateSelect();
-    validateLogo();
 }
 
 function closeModal() {
     modal.classList.remove('modal--visible');
     document.body.style.overflow = '';
     document.body.style.paddingRight = '';
+    cleanUpListeners();
 }
 
 function openFileDialog() {
@@ -54,24 +51,24 @@ function openFileDialog() {
 
 function handleFileUpload(event) {
     const file = event.target.files[0];
-    if (file) {
-        const allowedExtensions = /\.(jpe?g|png)$/i;
-        if (!allowedExtensions.test(file.name)) {
-            alert('Вы пытаетесь загрузить неподходящий файл. Пожалуйста, выберите картинку с расширением JPEG или PNG.');
-            logoUploadInput.value = '';
-            validateLogo();
-            return;
-        }
+    if (!file) return;
 
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            logoImage.src = e.target.result;
-            logoImage.alt = 'User Logo';
-            logoDeleteButton.classList.add('form__logo-delete-bg--visible');
-            validateLogo();
-        };
-        reader.readAsDataURL(file);
+    const allowedExtensions = /\.(jpe?g|png)$/i;
+    if (!allowedExtensions.test(file.name)) {
+        alert('Вы пытаетесь загрузить неподходящий файл. Пожалуйста, выберите картинку с расширением JPEG или PNG.');
+        logoUploadInput.value = '';
+        validateLogo();
+        return;
     }
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        logoImage.src = e.target.result;
+        logoImage.alt = 'User Logo';
+        logoDeleteButton.classList.add('form__logo-delete-bg--visible');
+        validateLogo();
+    };
+    reader.readAsDataURL(file);
 }
 
 function deleteLogo(event) {
@@ -81,14 +78,13 @@ function deleteLogo(event) {
     logoUploadInput.value = '';
     logoDeleteButton.classList.remove('form__logo-delete-bg--visible');
     validateLogo();
-    validateForm();
 }
 
 function validateOrganization() {
     const value = organizationInput.value.trim();
     isOrganizationValid = /^[A-Za-zА-Яа-я\s]{3,}$/.test(value);
 
-    const label = document.querySelector('label[for="organization-input"]');
+    const label = organizationInput.labels[0];
     if (label) {
         label.dataset.tooltip = isOrganizationValid
             ? 'Поле заполнено корректно'
@@ -107,7 +103,7 @@ function validatePhone() {
     const value = phoneInput.value.trim();
     isPhoneValid = /^\+7\d{10}$/.test(value);
 
-    const label = document.querySelector('label[for="phone-input"]');
+    const label = phoneInput.labels[0];
     if (label) {
         label.dataset.tooltip = isPhoneValid
             ? 'Поле заполнено корректно'
@@ -126,7 +122,7 @@ function validateEmail() {
     const value = emailInput.value.trim();
     isEmailValid = /^(?:[a-z\d!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z\d!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\u0001-\u0008\u000B\u000C\u000E-\u001F!\u0023-\u005B\u005D-\u007F]|\\[\u0001-\u0009\u000B\u000C\u000E-\u007F])*")@(?:(?:[a-z\d](?:[a-z\d-]*[a-z\d])?\.)+[a-z\d](?:[a-z\d-]*[a-z\d])?|\[(?:(?:25[0-5]|2[0-4]\d|[01]?\d{1,2})\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d{1,2}|[a-z\d-]*[a-z\d]:(?:[\u0001-\u0008\u000B\u000C\u000E-\u001F\u0021-\u005A\u0053-\u007F]|\\[\u0001-\u0009\u000B\u000C\u000E-\u007F])+)])$/.test(value);
     
-    const label = document.querySelector('label[for="email-input"]');
+    const label = emailInput.labels[0];
     if (label) {
         label.dataset.tooltip = isEmailValid
             ? 'Поле заполнено корректно'
@@ -145,7 +141,7 @@ function validateEmail() {
 function validateSelect() {
     isSelectValid = selectInput.value !== '';
 
-    const label = document.querySelector('label[for="form__select-input"]');
+    const label = selectInput.labels[0];
     if (label) {
         label.dataset.tooltip = isSelectValid
             ? 'Поле заполнено корректно'
@@ -185,15 +181,21 @@ function validateForm() {
     submitButton.disabled = !(isOrganizationValid && isPhoneValid && isEmailValid && isSelectValid && isLogoValid);
 }
 
-phoneInput.addEventListener('focus', () => {
+function formSubmit(event) {
+    if (!(isOrganizationValid && isPhoneValid && isEmailValid && isSelectValid && isLogoValid)) {
+        event.preventDefault();
+        alert('Каким-то магическим образом вам все же удалось нажать на кнопку отправки. К сожалению, ваши данные не будут отправлены, так как при заполнении формы были допущены ошибки (данные не валидны)');
+    }
+}
+
+function phoneFocus() {
     if (phoneInput.value.length <= 2) {
         phoneInput.value = '+7';
     }
-});
+}
 
-phoneInput.addEventListener('input', () => {
+function handlePhoneInput() {
     let value = phoneInput.value;
-
     value = '+7' + value.slice(2).replace(/\D/g, '');
   
     if (value.length > 12) {
@@ -202,50 +204,54 @@ phoneInput.addEventListener('input', () => {
 
     phoneInput.value = value;
     validatePhone();
-});
+}
 
-organizationInput.addEventListener('input', () => {
-    validateOrganization();
-})
-
-emailInput.addEventListener('input', () =>{
-    validateEmail();
-})
-
-selectInput.addEventListener('change', () => {
-    validateSelect();
-})
-
-logoUploadInput.addEventListener('change', () => {
-    validateLogo();
-});
-
-submitButton.addEventListener('click', (event) => {
-    if (!(isOrganizationValid && isPhoneValid && isEmailValid && isSelectValid && isLogoValid)) {
-        event.preventDefault();
-        alert('Каким-то магическим образом вам все же удалось нажать на кнопку отправки. К сожалению, ваши данные не будут отправлены, так как при заполнении формы были допущены ошибки (данные не валидны)');
-    }
-});
-
-document.addEventListener('mousedown', (event) => {
+function clickOutside(event) {
     if (event.target === modal) {
         closeModal();
     }
+}
+
+openModalButton.addEventListener('click', () => {
+    openModal();
+
+    document.addEventListener('mousedown', clickOutside);
+
+    phoneInput.addEventListener('focus', phoneFocus);
+    phoneInput.addEventListener('input', handlePhoneInput);
+
+    organizationInput.addEventListener('input', validateOrganization);
+    emailInput.addEventListener('input', validateEmail);
+    selectInput.addEventListener('change', validateSelect);
+    logoUploadInput.addEventListener('change', validateLogo);
+    
+    submitButton.addEventListener('click', formSubmit);
+
+    cancelFormButton.addEventListener('click', closeModal);
+    closeModalButton.addEventListener('click', closeModal);
+
+    logoUpdateBlock.addEventListener('click', openFileDialog);
+    logoUploadInput.addEventListener('change', handleFileUpload);
+    logoDeleteButton.addEventListener('click', deleteLogo);
 });
 
-document.querySelectorAll('[data-required-sign]').forEach((el) => {
-    el.textContent = '\u2217';
-});
+function cleanUpListeners() {
+    document.removeEventListener('mousedown', clickOutside);
 
-openModalButton.addEventListener('click', openModal);
-closeModalButton.addEventListener('click', closeModal);
-cancelFormButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    closeModal();
-});
+    phoneInput.removeEventListener('focus', phoneFocus);
+    phoneInput.removeEventListener('input', handlePhoneInput);
 
-logoUpdateBlock.addEventListener('click', openFileDialog);
-logoUploadInput.addEventListener('change', handleFileUpload);
-logoDeleteButton.addEventListener('click', deleteLogo);
+    organizationInput.removeEventListener('input', validateOrganization);
+    emailInput.removeEventListener('input', validateEmail);
+    selectInput.removeEventListener('change', validateSelect);
+    logoUploadInput.removeEventListener('change', validateLogo);
 
-validateForm();
+    submitButton.removeEventListener('click', formSubmit);
+
+    logoUpdateBlock.removeEventListener('click', openFileDialog);
+    logoUploadInput.addEventListener('change', handleFileUpload);
+    logoDeleteButton.addEventListener('click', deleteLogo);
+
+    cancelFormButton.removeEventListener('click', closeModal);
+    closeModalButton.removeEventListener('click', closeModal);
+}
